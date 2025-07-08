1ac408497ff873809686f5a5ac6e279b42985d53
AryanMahajan <mahajanaryan0409@gmail.com>
2025-07-08 18:06:29 +0530

    corporate-dataset-and-api-added

--- leak_data/urls.py
+++ leak_data/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path('get-password', views.get_passwords, name='get-passwords'),
     path('get-password', views.GetPassword.as_view(), name='get-passwords'),
     path('get-jobseeker', views.GetJobSeekerData.as_view(), name='get-jobseeker-data'),
-
+    path('get-corporate', views.GetCorporateData.as_view(), name='get-corporate-data')
 ]
--- leak_data/utils.py
+++ leak_data/utils.py
@@ -58,6 +58,26 @@ def get_jobseeker_dynamodb_table(table_name=None):
 
     return dynamodb_resource.Table(table_name)
 
+def get_corporate_dataset_dynamodb_table(table_name=None):
+    if dynamodb_resource is None:
+        initialize_dynamodb_clients()
+        if dynamodb_resource is None:
+            raise Exception("DynamoDB resource is not initialized. Check AWS configuration.")
+    if table_name is None:
+        table_name = settings.DYNAMODB_CORPORATE_TABLE_NAME
+
+    return dynamodb_resource.Table(table_name)
+
+def get_zomato_dataset_dynamodb_table(table_name=None):
+    if dynamodb_resource is None:
+        initialize_dynamodb_clients()
+        if dynamodb_resource is None:
+            raise Exception("DynamoDB resource is not initialized. Check AWS configuration.")
+    if table_name is None:
+        table_name = settings.DYNAMODB_ZOMATO_TABLE_NAME
+
+    return dynamodb_resource.Table(table_name)
+
 def get_dynamodb_client():
     """Returns the low-level DynamoDB client."""
     if dynamodb_client is None:
